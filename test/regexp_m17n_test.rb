a107e3016761dee6b8dde4aa25222ae2daa8cfc0
dp <d.phillipsds@gmail.com>
2014-08-04 20:34:59 +0800

    original

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -4,11 +4,7 @@ require_relative '../lib/regexp_m17n'
 
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
-    encodings = Encoding.list.reject do |enc|
-      enc == Encoding::ISO_2022_JP_2 ||
-      enc == Encoding::UTF_7
-    end
-    encodings.each do |enc|
+    Encoding.list.each do |enc|
       assert(RegexpM17N.non_empty?('.'.encode(enc)))
     end
   end
